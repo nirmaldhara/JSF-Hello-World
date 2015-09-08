8423b133ce6ab12dae3ec14dfc03f831bd82ead9
Nirmal Dhara <nirmal.dhara@gmail.com>
2015-09-08 19:57:03 +0530

    JSF – JavaServer Faces is a java MVC framework for web application. It help us to make user interface with less code and time.
    JSF – JavaServer Faces is a java MVC framework for web application. It
    help us to make user interface with less code and time.

---
+++ com.javaant/bean/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package bean;
+
+import java.io.Serializable;
+
+public class HelloWorldBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String fname;
+	private String lname;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+}
